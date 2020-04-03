1b732b973e034bef625daeb02a5e194e6215abf6
WinnieGotYu <WinnieGotYu@gmail.com>
2020-04-03 16:36:30 -0700

    mvp, added styled components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,51 @@
 import React, { useState } from "react";
 import Square from "./Square";
+import styled from "styled-components";
+
+const TicTacToeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 7.5vw;
+  text-align: center;
+  margin: 1rem 0 1rem 0;
+`;
+
+const Status = styled.h2`
+  font-size: 6vw;
+  text-align: center;
+  margin: 0 0 1rem 0;
+`;
+
+const BoardRow = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 20vw;
+`;
+
+const StartButton = styled.button`
+  font-size: 3vw;
+  padding: 1vh;
+  background: lightyellow;
+  border-radius: 5px;
+  width: 40vw;
+  font-weight: 600;
+  margin-top: 1rem;
+  margin-left: 20vw;
+`;
+
+const Img = styled.img`
+  height: 40vh;
+  width: 40vw;
+  margin-bottom: 1rem;
+`;
+const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const Board = () => {
   //boardState
@@ -8,8 +54,11 @@ const Board = () => {
   const [xIsNext, setXisNext] = useState(true);
 
   const handleClick = index => {
+    //make copy of board
     const squares = [...boardSquares];
+    //stop clicks on exisiting square and if game is won
     if (squares[index] || calculateWinner(boardSquares)) return;
+
     //add X or O
     squares[index] = xIsNext ? "X" : "O";
 
@@ -22,7 +71,11 @@ const Board = () => {
   //create a render square function
   const renderSquare = index => {
     return (
-      <Square value={boardSquares[index]} onClick={() => handleClick(index)} />
+      <Square
+        value={boardSquares[index]}
+        onClick={() => handleClick(index)}
+        winningIdx={winner}
+      />
     );
   };
   // take in an index
@@ -49,7 +102,7 @@ const Board = () => {
         squares[a] === squares[b] &&
         squares[b] === squares[c]
       ) {
-        // if so, return winner
+        // if so, return winner and indices of winning square
         return squares[a];
       }
     }
@@ -62,31 +115,47 @@ const Board = () => {
   status = winner
     ? `Winner is: ${winner}`
     : `Current Player: ${xIsNext ? "X" : "O"}`;
-  
-  //If board filled and no space remaining 
-  if(!boardSquares.includes(null)){
-    status = 'IT IS A TIE'
+
+  //If board filled and no space remaining
+  if (!boardSquares.includes(null) && !winner) {
+    status = "IT IS A TIE";
   }
 
+  const restart = () => {
+    setBoardSquares(Array(9).fill(null));
+  };
+
   return (
-    <div>
-      <div className="status">{status}</div>
-      <div>
+    <TicTacToeWrapper>
+      <Title>Tic Tac Toe</Title>
+      <Status>{status}</Status>
+      <ImgWrapper>
+        {winner ? (
+          <Img
+            src="https://media.giphy.com/media/t7sks6pQZmmBOJD1BT/giphy.gif"
+            alt="Winning Gif"
+          />
+        ) : (
+          <div />
+        )}
+      </ImgWrapper>
+      <BoardRow>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div>
+      </BoardRow>
+      <BoardRow>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div>
+      </BoardRow>
+      <BoardRow>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-      </div>
-    </div>
+      </BoardRow>{" "}
+      <StartButton onClick={() => restart()}> New Game</StartButton>
+    </TicTacToeWrapper>
   );
 };
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const SquareButton = styled.button`
+  width: 20%;
+  font-size: 4rem;
+  border: 2px solid black;
+  border-radius: 7px;
+  text-align: center;
+  padding: 0;
+  background: none;
+  cursor: pointer;
+`;
 
 const Square = props => {
-  return <button onClick={props.onClick}>{props.value}</button>;
+  return (
+    <SquareButton
+      onClick={props.onClick}
+    >
+      {props.value}
+    </SquareButton>
+  );
 };
 
 export default Square;
